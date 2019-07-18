cefc221cc66b2a7e10ac9603e22b63faf3abd32f
Aravind Rajeswaran <rajeswaran.aravind@gmail.com>
2019-07-18 01:09:52 -0700

    behavior cloning redesign

--- mjrl/algos/behavior_cloning.py
+++ mjrl/algos/behavior_cloning.py
@@ -1,3 +1,7 @@
+"""
+Minimize bc loss (MLE, MSE, RWR etc.) with pytorch optimizers
+"""
+
 import logging
 logging.disable(logging.CRITICAL)
 import numpy as np
@@ -8,61 +12,113 @@ from torch.autograd import Variable
 from mjrl.utils.logger import DataLog
 from tqdm import tqdm
 
+
 class BC:
     def __init__(self, expert_paths,
                  policy,
                  epochs = 5,
                  batch_size = 64,
                  lr = 1e-3,
-                 optimizer = None):
+                 optimizer = None,
+                 loss_type = 'MLE',  # can be 'MLE' or 'MSE'
+                 ):
 
         self.policy = policy
         self.expert_paths = expert_paths
         self.epochs = epochs
         self.mb_size = batch_size
         self.logger = DataLog()
+        self.loss_type = loss_type
 
-        # get transformations
-        observations = np.concatenate([path["observations"] for path in expert_paths])
-        actions = np.concatenate([path["actions"] for path in expert_paths])
-        in_shift, in_scale = np.mean(observations, axis=0), np.std(observations, axis=0)
-        out_shift, out_scale = np.mean(actions, axis=0), np.std(actions, axis=0)
-
-        # set scalings in the target policy
-        self.policy.model.set_transformations(in_shift, in_scale, out_shift, out_scale)
-        self.policy.old_model.set_transformations(in_shift, in_scale, out_shift, out_scale)
+        in_shift, in_scale, out_shift, out_scale = self.compute_transformations()
+        self.set_transformations(in_shift, in_scale, out_shift, out_scale)
 
         # construct optimizer
         self.optimizer = torch.optim.Adam(self.policy.trainable_params, lr=lr) if optimizer is None else optimizer
 
-    def loss(self, obs, act):
+        # Loss criterion if required
+        if loss_type == 'MSE':
+            self.loss_criterion = torch.nn.MSELoss()
+
+    def compute_transformations(self):
+        # get transformations
+        if self.expert_paths == [] or self.expert_paths is None:
+            in_shift, in_scale, out_shift, out_scale = None, None, None, None
+        else:
+            observations = np.concatenate([path["observations"] for path in self.expert_paths])
+            actions = np.concatenate([path["actions"] for path in self.expert_paths])
+            in_shift, in_scale = np.mean(observations, axis=0), np.std(observations, axis=0)
+            out_shift, out_scale = np.mean(actions, axis=0), np.std(actions, axis=0)
+        return in_shift, in_scale, out_shift, out_scale
+
+    def set_transformations(self, in_shift=None, in_scale=None, out_shift=None, out_scale=None):
+        # set scalings in the target policy
+        self.policy.model.set_transformations(in_shift, in_scale, out_shift, out_scale)
+        self.policy.old_model.set_transformations(in_shift, in_scale, out_shift, out_scale)
+
+    def set_variance_with_data(self, out_scale):
+        # set the variance of gaussian policy based on out_scale
+        params = self.policy.get_param_values()
+        params[-self.policy.m:] = np.log(out_scale + 1e-12)
+        self.policy.set_param_values(params)
+
+    def loss(self, data, idx=None):
+        if self.loss_type == 'MLE':
+            return self.mle_loss(data, idx)
+        elif self.loss_type == 'MSE':
+            return self.mse_loss(data, idx)
+        else:
+            print("Please use valid loss type")
+            return None
+
+    def mle_loss(self, data, idx):
+        # use indices if provided (e.g. for mini-batching)
+        # otherwise, use all the data
+        idx = range(data['observations'].shape[0]) if idx is None else idx
+        obs = data['observations'][idx]
+        act = data['expert_actions'][idx]
         LL, mu, log_std = self.policy.new_dist_info(obs, act)
         # minimize negative log likelihood
         return -torch.mean(LL)
 
-    def train(self):
-        observations = np.concatenate([path["observations"] for path in self.expert_paths])
-        actions = np.concatenate([path["actions"] for path in self.expert_paths])
+    def mse_loss(self, data, idx=None):
+        idx = range(data['observations'].shape[0]) if idx is None else idx
+        obs = data['observations'][idx]
+        obs = Variable(torch.from_numpy(obs).float(), requires_grad=False)
+        act_expert = data['expert_actions'][idx]
+        act_expert = Variable(torch.from_numpy(act_expert).float(), requires_grad=False)
+        act_pi = self.policy.model(obs)
+        return self.loss_criterion(act_pi, act_expert.detach())
 
-        params_before_opt = self.policy.get_param_values()
+    def fit(self, data, log_info=True):
+        # data is a dict
+        # keys should have "observations" and "expert_actions"
+        validate_keys = all([k in data.keys() for k in ["observations", "expert_actions"]])
+        assert validate_keys is True
         ts = timer.time()
-        num_samples = observations.shape[0]
+        num_samples = data["observations"].shape[0]
         for ep in tqdm(range(self.epochs)):
-            self.logger.log_kv('epoch', ep)
-            loss_val = self.loss(observations, actions).data.numpy().ravel()[0]
-            self.logger.log_kv('loss', loss_val)
-            self.logger.log_kv('time', (timer.time()-ts))
+            if log_info is True:
+                self.logger.log_kv('epoch', ep)
+                loss_val = self.loss(data, idx=range(num_samples)).data.numpy().ravel()[0]
+                self.logger.log_kv('loss', loss_val)
+                self.logger.log_kv('time', (timer.time()-ts))
             for mb in range(int(num_samples / self.mb_size)):
                 rand_idx = np.random.choice(num_samples, size=self.mb_size)
-                obs = observations[rand_idx]
-                act = actions[rand_idx]
                 self.optimizer.zero_grad()
-                loss = self.loss(obs, act)
+                loss = self.loss(data, idx=rand_idx)
                 loss.backward()
                 self.optimizer.step()
         params_after_opt = self.policy.get_param_values()
         self.policy.set_param_values(params_after_opt, set_new=True, set_old=True)
-        self.logger.log_kv('epoch', self.epochs)
-        loss_val = self.loss(observations, actions).data.numpy().ravel()[0]
-        self.logger.log_kv('loss', loss_val)
-        self.logger.log_kv('time', (timer.time()-ts))
+        if log_info is True:
+            self.logger.log_kv('epoch', self.epochs)
+            loss_val = self.loss(data, idx=range(num_samples)).data.numpy().ravel()[0]
+            self.logger.log_kv('loss', loss_val)
+            self.logger.log_kv('time', (timer.time()-ts))
+
+    def train(self):
+        observations = np.concatenate([path["observations"] for path in self.expert_paths])
+        expert_actions = np.concatenate([path["actions"] for path in self.expert_paths])
+        data = dict(observations=observations, expert_actions=expert_actions)
+        self.fit(data, log_info=True)
--- mjrl/algos/behavior_cloning_2.py
+++ mjrl/algos/behavior_cloning_2.py
@@ -7,8 +7,10 @@ import torch.nn as nn
 from torch.autograd import Variable
 from mjrl.utils.logger import DataLog
 from tqdm import tqdm
+from mjrl.algos.behavior_cloning import BC as BaseBC
 
-class BC:
+
+class BC(BaseBC):
     def __init__(self, expert_paths,
                  policy,
                  epochs = 5,
@@ -22,20 +24,8 @@ class BC:
         self.mb_size = batch_size
         self.logger = DataLog()
 
-        # get transformations
-        observations = np.concatenate([path["observations"] for path in expert_paths])
-        actions = np.concatenate([path["actions"] for path in expert_paths])
-        in_shift, in_scale = np.mean(observations, axis=0), np.std(observations, axis=0)
-        out_shift, out_scale = np.mean(actions, axis=0), np.std(actions, axis=0)
-
-        # set scalings in the target policy
-        self.policy.model.set_transformations(in_shift, in_scale, out_shift, out_scale)
-        self.policy.old_model.set_transformations(in_shift, in_scale, out_shift, out_scale)
-
-        # set the variance of gaussian policy based on out_scale
-        params = self.policy.get_param_values()
-        params[-self.policy.m:] = np.log(out_scale + 1e-12)
-        self.policy.set_param_values(params)
+        in_shift, in_scale, out_shift, out_scale = self.compute_transformations()
+        self.set_transformations(in_shift, in_scale, out_shift, out_scale)
 
         # construct optimizer
         self.optimizer = torch.optim.Adam(self.policy.model.parameters(), lr=lr) if optimizer is None else optimizer
@@ -43,35 +33,11 @@ class BC:
         # loss criterion is MSE for maximum likelihood estimation
         self.loss_function = torch.nn.MSELoss()
 
-    def loss(self, obs, act):
-        obs_var = Variable(torch.from_numpy(obs).float(), requires_grad=False)
-        act_var = Variable(torch.from_numpy(act).float(), requires_grad=False)
-        act_hat = self.policy.model(obs_var)
-        return self.loss_function(act_hat, act_var.detach())
-
-    def train(self):
-        observations = np.concatenate([path["observations"] for path in self.expert_paths])
-        actions = np.concatenate([path["actions"] for path in self.expert_paths])
-
-        params_before_opt = self.policy.get_param_values()
-        ts = timer.time()
-        num_samples = observations.shape[0]
-        for ep in tqdm(range(self.epochs)):
-            self.logger.log_kv('epoch', ep)
-            loss_val = self.loss(observations, actions).data.numpy().ravel()[0]
-            self.logger.log_kv('loss', loss_val)
-            self.logger.log_kv('time', (timer.time()-ts))
-            for mb in range(int(num_samples / self.mb_size)):
-                rand_idx = np.random.choice(num_samples, size=self.mb_size)
-                obs = observations[rand_idx]
-                act = actions[rand_idx]
-                self.optimizer.zero_grad()
-                loss = self.loss(obs, act)
-                loss.backward()
-                self.optimizer.step()
-        params_after_opt = self.policy.get_param_values()
-        self.policy.set_param_values(params_after_opt, set_new=True, set_old=True)
-        self.logger.log_kv('epoch', self.epochs)
-        loss_val = self.loss(observations, actions).data.numpy().ravel()[0]
-        self.logger.log_kv('loss', loss_val)
-        self.logger.log_kv('time', (timer.time()-ts))
+    def loss(self, data, idx=None):
+        idx = range(data['observations'].shape[0]) if idx is None else idx
+        obs = data['observations'][idx]
+        obs = Variable(torch.from_numpy(obs).float(), requires_grad=False)
+        act_expert = data['expert_actions'][idx]
+        act_expert = Variable(torch.from_numpy(act_expert).float(), requires_grad=False)
+        act_pi = self.policy.model(obs)
+        return self.loss_function(act_pi, act_expert.detach())
