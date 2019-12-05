4538fb80a98638f45f6701b1e3f35ca2dea7e484
Ben Evans <bevans97@cs.washington.edu>
2019-12-05 00:59:39 -0800

    fixed multithreaded gpu sampling

--- tests/point_mass_test_cpu.py
+++ tests/point_mass_test_cpu.py
@@ -11,22 +11,18 @@ SEED = 500
 HIDDEN_SIZE = 1024
 
 e = GymEnv('mjrl_point_mass-v0')
-policy = MLP(e.spec, hidden_sizes=(HIDDEN_SIZE, HIDDEN_SIZE), seed=SEED, device='cuda')
+policy = MLP(e.spec, hidden_sizes=(HIDDEN_SIZE, HIDDEN_SIZE), seed=SEED)
 baseline = MLPBaseline(e.spec, reg_coef=1e-3, batch_size=64, epochs=10, learn_rate=1e-3)
 agent = NPG(e, policy, baseline, normalized_step_size=0.05, seed=SEED, save_logs=True)
 
-assert policy.device == 'cuda'
-assert policy.model.device == 'cuda'
-assert policy.model.fc_layers[0].weight.device.type == 'cuda'
-
 ts = timer.time()
-train_agent(job_name='point_mass_gpu_exp1',
+train_agent(job_name='point_mass_exp1',
             agent=agent,
             seed=SEED,
             niter=50,
             gamma=0.95,
             gae_lambda=0.97,
-            num_cpu=4,
+            num_cpu=1,
             sample_mode='trajectories',
             num_traj=40,      # samples = 40*25 = 1000
             save_freq=5,
